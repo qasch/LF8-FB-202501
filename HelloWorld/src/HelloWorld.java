10f02fb6445e7895266928cbf7d9857b6529dbd0
Kai Schell <kai.schell@oscert.eu>
2025-01-17 15:51:40 +0100

    simple HelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    // Main Methode
+    // wird automatisch ausgeführt
+   public static void main(String[] args) {
+       // Ausgabe auf der Konsole
+       System.out.println("Hello World");
+
+       // Unterschied von println und print
+       System.out.println("Jaja, auch Hello IntelliJ");
+       System.out.print("Dies ist eine Ausgabe mit print, nicht mit println");
+       System.out.print("Ja wo ist denn der Unterschied?\n");  // \n erzeugt einen Zeilenumbruch
+       System.out.print("Aha, es fehlt der Zeilenumbruch");
+   }
+}
+
