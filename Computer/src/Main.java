b6cca1e8a405f4e87a54842112d7b757d36b92f1
Kai Schell <kai.schell@oscert.eu>
2025-01-24 13:03:14 +0100

    add Computer

--- Computer/src/Main.java
+++ Computer/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Computer c = new Computer(1234, "Gretl", 3);
+        Computer c = new Computer(1234, "Gretl", 9);
         System.out.println(c.getSERIENNUMMER());
         System.out.println(c.getBesitzer());
         System.out.println(c.getSpeicherplatz());
 
-        c.speicherHinzufuegen(-40);
+        c.speicherHinzufuegen(-1);
         System.out.println(c.getSpeicherplatz());
 
         Computer d = new Computer(6789, "Kai", -1);
